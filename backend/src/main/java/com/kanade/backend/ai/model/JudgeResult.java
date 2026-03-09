84a2562a886c2aee4fbe563d01628fe7d0fdfe33
kanade <jrdrywnt.163.com>
2026-03-09 09:04:45 +0800

    完成 AI 自动标注知识点、学科

--- backend/src/main/java/com/kanade/backend/ai/model/JudgeResult.java
+++ backend/src/main/java/com/kanade/backend/ai/model/JudgeResult.java
@@ -1,5 +1,10 @@
 package com.kanade.backend.ai.model;
 
+import dev.langchain4j.model.output.structured.Description;
+import lombok.Data;
+
+@Data
+@Description("")
 public class JudgeResult {
     Integer score;
     String reason;
