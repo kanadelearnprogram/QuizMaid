9c4766788fd8f887800fd772553263ed797fd54d
kanade <jrdrywnt@163.com>
2026-03-18 21:07:39 +0800

    Revert "试题Excel批量导入 (#49)"
    This reverts commit 33f6703d2d739cc5010ca45ddf1f259dd046af12.

--- backend/src/main/java/com/kanade/backend/config/RedissonConfig.java
+++ backend/src/main/java/com/kanade/backend/config/RedissonConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "spring.data.redis")
+@ConfigurationProperties(prefix = "spring.redis")
 @Data
 public class RedissonConfig {
 
